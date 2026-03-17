e9a3391f52f20dd4e9d7c96e35e4e1a73ba1269e
Indra2512 <mononeon1225@gmail.com>
2026-03-17 17:15:07 +0700

    file js dan css

---
+++ Praktikum.js
@@ -0,0 +1,15 @@
+//Date() untuk menampilkan tanggal saat ini
+Date()
+
+//Mendapatkan tanggal hari ini
+let tanggal = new Date()
+
+//untuk menampilkan alert tampil()
+function tampil(){
+    alert("Ini adalah button")
+}
+
+//untuk menampilkan alert show()
+function show(){
+    alert("ini adalah pop up utnuk tulisan perkenalan")
+}
