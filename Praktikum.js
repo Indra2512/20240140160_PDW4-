b7c9e9e01260a34b32215a301dfcb60e64d74f83
Indra2512 <mononeon1225@gmail.com>
2026-03-17 23:04:58 +0700

    update code

--- Praktikum.js
+++ Praktikum.js
@@ -1,15 +1,35 @@
-//Date() untuk menampilkan tanggal saat ini
-Date()
+let tanggal = new Date();
+console.log("Halaman dibuka pada: " + tanggal);
 
-//Mendapatkan tanggal hari ini
-let tanggal = new Date()
-
-//untuk menampilkan alert tampil()
-function tampil(){
-    alert("Ini adalah button")
+function tampil() {
+    alert("Ini adalah button");
 }
 
-//untuk menampilkan alert show()
-function show(){
-    alert("ini adalah pop up utnuk tulisan perkenalan")
+function show() {
+    alert("ini adalah pop up untuk tulisan perkenalan");
+    
+    
+    let deskripsi = document.querySelector('.deskripsi');
+    if (deskripsi.style.display === "none") {
+        deskripsi.style.display = "block";
+    } else {
+        deskripsi.style.display = "none";
+    }
+}
+
+function toggleImages(namaClass) {
+    
+    let daftarGambar = document.querySelectorAll('.' + namaClass);
+    
+    daftarGambar.forEach(img => {
+        
+        let displayAsli = window.getComputedStyle(img).display;
+
+        if (displayAsli === "none") {
+            img.style.display = "block";  
+            img.style.margin = "10px auto"; 
+        } else {
+            img.style.display = "none";  
+        }
+    });
 }
