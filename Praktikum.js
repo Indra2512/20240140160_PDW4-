44b610ed43caac504e870c715efba4fb94fd2750
Indra2512 <mononeon1225@gmail.com>
2026-03-18 00:25:27 +0700

    rombak file

--- Praktikum.js
+++ Praktikum.js
@@ -1,35 +1,27 @@
 let tanggal = new Date();
-console.log("Halaman dibuka pada: " + tanggal);
 
-function tampil() {
+function tampil(){
     alert("Ini adalah button");
 }
 
-function show() {
+function show(){
     alert("ini adalah pop up untuk tulisan perkenalan");
-    
-    
-    let deskripsi = document.querySelector('.deskripsi');
-    if (deskripsi.style.display === "none") {
-        deskripsi.style.display = "block";
-    } else {
-        deskripsi.style.display = "none";
-    }
 }
 
-function toggleImages(namaClass) {
+function toggleGrup(idElemen) {
+    let grup = document.getElementById(idElemen);
     
-    let daftarGambar = document.querySelectorAll('.' + namaClass);
-    
-    daftarGambar.forEach(img => {
-        
-        let displayAsli = window.getComputedStyle(img).display;
-
-        if (displayAsli === "none") {
-            img.style.display = "block";  
-            img.style.margin = "10px auto"; 
-        } else {
-            img.style.display = "none";  
-        }
-    });
+ 
+    if (grup.classList.contains("tampil-animasi")) {
+        grup.classList.remove("tampil-animasi");
+        grup.style.display = "none";
+    } 
+ 
+    else {
+        grup.style.display = "block";
+       
+        setTimeout(() => {
+            grup.classList.add("tampil-animasi");
+        }, 10);
+    }
 }
